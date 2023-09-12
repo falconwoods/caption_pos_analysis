6a5a8c51a8e5974123b394ce0bb68d13dc9c4ead
eric <walkcloud@outlook.com>
2023-09-12 16:52:42 -0700

    pos types

---
+++ pos_types.py
@@ -0,0 +1,47 @@
+cpdef enum univ_pos_t:
+    NO_TAG = 0
+    ADJ = symbols.ADJ
+    ADP
+    ADV
+    AUX
+    CONJ
+    CCONJ  # U20
+    DET
+    INTJ
+    NOUN
+    NUM
+    PART
+    PRON
+    PROPN
+    PUNCT
+    SCONJ
+    SYM
+    VERB
+    X
+    EOL
+    SPACE
+
+IDS = {
+    "": NO_TAG,
+    "ADJ": ADJ,
+    "ADP": ADP,
+    "ADV": ADV,
+    "AUX": AUX,
+    "CONJ": CONJ,  # U20
+    "CCONJ": CCONJ,
+    "DET": DET,
+    "INTJ": INTJ,
+    "NOUN": NOUN,
+    "NUM": NUM,
+    "PART": PART,
+    "PRON": PRON,
+    "PROPN": PROPN,
+    "PUNCT": PUNCT,
+    "SCONJ": SCONJ,
+    "SYM": SYM,
+    "VERB": VERB,
+    "X": X,
+    "EOL": EOL,
+    "SPACE": SPACE
+}
+
